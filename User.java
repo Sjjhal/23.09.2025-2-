53e3cf2a2f2466ba72688a76e148c494627ebc5b
Sjjhal <lolk57462@gmail.com>
2025-10-04 02:17:08 +0300

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,34 @@
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String login;
+    private String password;
+    private String role;
 
+    // Конструктор
+    public User(int id, String firstName, String lastName, String login, String password, String role) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    // Методи (поки що пусті)
+    public void register() {}
+    public void login() {}
+    public void addToFavorites(Book book) {}
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", login='" + login + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
