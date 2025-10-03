c7aa9e689fb3d54d46717e281ac80df51312e71b
Sjjhal <lolk57462@gmail.com>
2025-10-04 02:15:37 +0300

    Update LibraryCatalog.java

--- LibraryCatalog.java
+++ LibraryCatalog.java
@@ -1,1 +1,18 @@
+public class LibraryCatalog {
+    private String name;
+
+    public LibraryCatalog(String name) {
+        this.name = name;
+    }
+
+    public void searchByTitle(String title) {}
+    public void searchByAuthor(String author) {}
+
+    @Override
+    public String toString() {
+        return "LibraryCatalog{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
 
