bd6968312ced8778d0238a571018f34c5f59a7e8
Sjjhal <lolk57462@gmail.com>
2025-10-04 02:14:46 +0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,1 +1,32 @@
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int year;
+    private String genre;
+    private String type;
 
+    public Book(int id, String title, String author, int year, String genre, String type) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+        this.type = type;
+    }
+
+    public void open() {}
+    public void read() {}
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", genre='" + genre + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
