216875f9d74922e531c8ed1a949e80268008e268
Sjjhal <lolk57462@gmail.com>
2025-10-04 02:15:58 +0300

    Update Download.java

--- Download.java
+++ Download.java
@@ -1,1 +1,25 @@
+public class Download {
+    private int id;
+    private User user;
+    private Book book;
+    private String date;
 
+    public Download(int id, User user, Book book, String date) {
+        this.id = id;
+        this.user = user;
+        this.book = book;
+        this.date = date;
+    }
+
+    public void startDownload() {}
+
+    @Override
+    public String toString() {
+        return "Download{" +
+                "id=" + id +
+                ", user=" + user +
+                ", book=" + book +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
